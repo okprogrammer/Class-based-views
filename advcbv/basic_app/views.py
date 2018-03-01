de2b8abde0963a2f08f8edf8982caf99731f515c
okprogrammmer <omk7912@gmail.com>
2018-03-01 22:03:06 +0530

    inhereting files

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
 from . import models
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
 
 class SchoolListView(ListView):
-    model=models.School
+    context_object_name = 'schools'
+    model=models.School     #this by default return school_list
+
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
